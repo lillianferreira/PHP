b1287abb7beabcd98a3252f7d2c89d2cd3713dfe
lillianferreira <lillian_af@hotmail.com>
2022-11-26 08:56:13 -0300

    Aula 04 e 05 PHP

---
+++ aula05/exemp002.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+    <meta charset="UTF-8"/>
+    <link rel="stylesheet" href="_css/estilo.css">
+    <title>XXXX</title>
+</head>
+<body>
+<div>
+    <?php
+        $n1 = 3;
+        $n2 = 2;
+        $s = $n1 + $n2;
+        echo "A soma entre $n1 e $n2 é igual a $s;
+    ?>
+</div>
+</body>
