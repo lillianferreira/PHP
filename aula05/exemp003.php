817b7de16264f0ad8613b960d2e31515c89ba7bc
lillianferreira <lillian_af@hotmail.com>
2022-11-30 09:29:52 -0300

    exemp003.php

--- aula05/exemp003.php
+++ aula05/exemp003.php
@@ -17,7 +17,7 @@
 	<?php
 		$v1 = $_GET["x"];
     	$v2 = $_GET["y"];
-    	echo "<h2>Valores recbidos: $v1 e $v2</h2>;
+    	echo "<h2>Valores recebidos: $v1 e $v2</h2>;
     	echo "O valor absoluto de $v2 e ". abs($v2);
    		echo "<br/>O valor de $v1<sup>$v2</sup> e " . pow($v1, $v2);
     	echo "<br/>A raiz de $v1 e " . sqrt($v1);
